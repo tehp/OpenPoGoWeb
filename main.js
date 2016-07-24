5b873e7d0c1005ff94a96409d91b978353e21d48
Reaver01 <reaver_01@gmx.com>
2016-07-24 15:43:03 +0200

    Update 3
    main.css
    - Added: class for small images
    main.js
    - Changed: stats array is built by function
    - Changed: run addInventory() instead of looping through buildMenu()
    - Added: function to create arrays based on username
    - Changed: invSuccess needs to use the user_index for multibot to work
    - Changed: Menus now have tabs for each bot running
    - Added: More stats to be displayed in pokemon menu: CP, Moves,  IVs
    Pokeball used

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ var inventory = {};
 var playerInfo = {};
 var pokedex = {};
 var pokemonArray = {};
-var stats = {};
+var stats = makeArrays();
 var user_data = {};
 var itemsArray = {
   '0': 'Unknown',
@@ -95,9 +95,7 @@ function initMap() {
   setTimeout(function(){
     placeTrainer();
     addCatchable();
-    for (menu = 1; menu < 5; menu++) {
-      buildMenu();
-    }
+    addInventory();
     setTimeout(function(){
       setInterval(updateTrainer, 1000);
       setInterval(addCatchable, 1000);
@@ -186,12 +184,20 @@ var errorFunc = function(xhr) {
   console.error(xhr);
 };
 
+function makeArrays() {
+  var tempArr = {};
+  for(i = 0; i < users.length; i ++) {
+    tempArr[users[i]] = {};
+  }
+  return tempArr;
+}
+
 var invSuccess = function(data, user_index) {
-  bagCandy = filter(data, 'pokemon_family');
-  bagItems = filter(data, 'item');
-  bagPokemon = filter(data, 'pokemon_data');
-  pokedex = filter(data, 'pokedex_entry');
-  stats = filter(data, 'player_stats');
+  bagCandy[users[user_index]] = filter(data, 'pokemon_family');
+  bagItems[users[user_index]] = filter(data, 'item');
+  bagPokemon[users[user_index]] = filter(data, 'pokemon_data');
+  pokedex[users[user_index]] = filter(data, 'pokedex_entry');
+  stats[users[user_index]] = filter(data, 'player_stats');
 };
 
 var trainerFunc = function(data, user_index) {
@@ -377,100 +383,184 @@ function buildMenu() {
   addInventory();
   if (menu == 1) {
     document.getElementById('subtitle').innerHTML = 'Trainer Info';
-    out = '';
-    for (var i = 0; i < stats.length; i++) {
-      out += '<div class="row"><div class="col s12"><h5>' +
-              users[0] +
-              '</h5><br>Level: ' +
-              stats[i].inventory_item_data.player_stats.level +
-              '<br>Exp: ' +
-              stats[i].inventory_item_data.player_stats.experience +
-              '<br>Exp to Lvl ' +
-              ( parseInt(stats[i].inventory_item_data.player_stats.level, 10) + 1 ) +
-              ': ' +
-              (parseInt(stats[i].inventory_item_data.player_stats.next_level_xp, 10) - stats[i].inventory_item_data.player_stats.experience) +
-              '<br>Pokemon Encountered: ' +
-              stats[i].inventory_item_data.player_stats.pokemons_encountered +
-              '<br>Pokeballs Thrown: ' +
-              stats[i].inventory_item_data.player_stats.pokeballs_thrown +
-              '<br>Pokemon Caught: ' +
-              stats[i].inventory_item_data.player_stats.pokemons_captured +
-              '<br>Small Ratata Caught: ' +
-              stats[i].inventory_item_data.player_stats.small_rattata_caught +
-              '<br>Pokemon Evolved: ' +
-              stats[i].inventory_item_data.player_stats.evolutions +
-              '<br>Eggs Hatched: ' +
-              stats[i].inventory_item_data.player_stats.eggs_hatched +
-              '<br>Unique Pokedex Entries: ' +
-              stats[i].inventory_item_data.player_stats.unique_pokedex_entries +
-              '<br>PokeStops Visited: ' +
-              stats[i].inventory_item_data.player_stats.poke_stop_visits +
-              '<br>Kilometers Walked: ' +
-              parseFloat(stats[i].inventory_item_data.player_stats.km_walked).toFixed(2) +
-              '</div></div>';
+    out = '<div class="row"><div class="col s12"><ul class="tabs">';
+    for (i = 0; i < users.length; i++) {
+      out += '<li class="tab col s3"><a href="#tabI' +
+              users[i] +
+              '">' +
+              users[i] +
+              '</a></li>';
     }
+    out += '</ul></div>';
+    for (i = 0; i < users.length; i++) {
+      out += '<div id="tabI' +
+              users[i] +
+              '" class="col s12"><h5>' +
+              users[i] +
+              '</h5><br>Level: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.level +
+              '<br>Exp: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.experience +
+              '<br>Exp to Lvl ' +
+              ( parseInt(stats[users[i]][0].inventory_item_data.player_stats.level, 10) + 1 ) +
+              ': ' +
+              (parseInt(stats[users[i]][0].inventory_item_data.player_stats.next_level_xp, 10) - stats[users[i]][0].inventory_item_data.player_stats.experience) +
+              '<br>Pokemon Encountered: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.pokemons_encountered +
+              '<br>Pokeballs Thrown: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.pokeballs_thrown +
+              '<br>Pokemon Caught: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.pokemons_captured +
+              '<br>Small Ratata Caught: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.small_rattata_caught +
+              '<br>Pokemon Evolved: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.evolutions +
+              '<br>Eggs Hatched: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.eggs_hatched +
+              '<br>Unique Pokedex Entries: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.unique_pokedex_entries +
+              '<br>PokeStops Visited: ' +
+              stats[users[i]][0].inventory_item_data.player_stats.poke_stop_visits +
+              '<br>Kilometers Walked: ' +
+              parseFloat(stats[users[i]][0].inventory_item_data.player_stats.km_walked).toFixed(2) +
+              '</div>';
+    }
+    out += '</div>';
     document.getElementById('subcontent').innerHTML = out;
+    $('ul.tabs').tabs();
   }
   if (menu == 2) {
     document.getElementById('subtitle').innerHTML = "Items in Bag";
-    out = '<div class="row items"><div class="col s12"><h5>' + users[0] + '</h5>';
-    for (i = 0; i < bagItems.length; i++) {
-      out += '<table><tr><td><img src="image/items/' +
-              bagItems[i].inventory_item_data.item.item_id +
-              '.png" class="item_img"></td><td>Item: ' +
-              itemsArray[bagItems[i].inventory_item_data.item.pokemon_id] +
-              '<br>Count: ' +
-              bagItems[i].inventory_item_data.item.count +
-              '</td>';
+    out = '<div class="row"><div class="col s12"><ul class="tabs">';
+    for (i = 0; i < users.length; i++) {
+      out += '<li class="tab col s3"><a href="#tabB' +
+              users[i] +
+              '">' +
+              users[i] +
+              '</a></li>';
     }
-    out += '</tr></table></div></div>';
+    out += '</ul></div>';
+    for (i = 0; i < users.length; i++) {
+      out += '<div id="tabB' +
+                users[i] +
+                '" class="col s12 items"><h5>' + users[i] + '</h5>';
+      for (x = 0; x < bagItems[users[i]].length; x++) {
+        out += '<table><tr><td><img src="image/items/' +
+                bagItems[users[i]][x].inventory_item_data.item.item_id +
+                '.png" class="item_img"></td><td>Item: ' +
+                itemsArray[bagItems[users[i]][x].inventory_item_data.item.item_id] +
+                '<br>Count: ' +
+                bagItems[users[i]][x].inventory_item_data.item.count +
+                '</td>';
+      }
+      out += '</tr></table></div>';
+    }
+    out += '</div>';
     document.getElementById('subcontent').innerHTML = out;
+    $('ul.tabs').tabs();
   }
   if (menu == 3) {
     document.getElementById('subtitle').innerHTML = "Pokemon in Bag";
-    pkmnTotal = bagPokemon.length;
-    out = '<div class="row items"><div class="col s12"><h5>' +
-          users[0] +
-          ' | ' +
-          pkmnTotal +
-          ' Pokemon</h5><table>';
-    for (i = 0; i < bagPokemon.length; i++) {
-      if (bagPokemon[i].inventory_item_data.pokemon_data.is_egg) {
-        pkmnNum = "???";
-        pkmnImage = "Egg.png";
-        pkmnName = "Egg";
-      } else {
-        pkmnNum = bagPokemon[i].inventory_item_data.pokemon_data.pokemon_id;
-        pkmnImage = pad_with_zeroes(bagPokemon[i].inventory_item_data.pokemon_data.pokemon_id, 3) + '.png';
-        pkmnName = pokemonArray[pkmnNum-1].Name;
-      }
-      out += '<tr><td><img src="image/pokemon/' + pkmnImage + '" class="png_img"></td><td class="left-align">Name: ' + pkmnName +
-      '<br>Number: ' + pkmnNum + '</td></tr>';
+    out = '<div class="col s12"><ul class="tabs">';
+    for (i = 0; i < users.length; i++) {
+      out += '<li class="tab col s3"><a href="#tabBP' +
+              users[i] +
+              '">' +
+              users[i] +
+              '</a></li>';
     }
-    out += '</table></div></div>';
+    out += '</ul></div>';
+    for (i = 0; i < users.length; i++) {
+      pkmnTotal = bagPokemon[users[i]].length;
+      out += '<div id="tabBP' +
+            users[i] +
+            '" class="col s12 items"><h5>' +
+            users[i] +
+            ' | ' +
+            pkmnTotal +
+            ' Pokemon</h5><table>';
+      for (x = 0; x < bagPokemon[users[i]].length; x++) {
+        if (bagPokemon[users[i]][x].inventory_item_data.pokemon_data.is_egg) {
+          pkmnNum = '???';
+          pkmnImage = 'Egg.png';
+          pkmnName = 'Egg';
+          pkmnCP = '';
+          pkmnMove1 = '';
+          pkmnMove2 = '';
+          ivList = '<td></td>';
+          pokeballUsed + '<td></td>';
+        } else {
+          pkmnNum = bagPokemon[users[i]][x].inventory_item_data.pokemon_data.pokemon_id;
+          pkmnImage = pad_with_zeroes(bagPokemon[users[i]][x].inventory_item_data.pokemon_data.pokemon_id, 3) + '.png';
+          pkmnName = pokemonArray[pkmnNum-1].Name;
+          pkmnCP = '<br>CP: ' + bagPokemon[users[i]][x].inventory_item_data.pokemon_data.cp;
+          pkmnMove1 = '<br>Move 1: ' + bagPokemon[users[i]][x].inventory_item_data.pokemon_data.move_1;
+          pkmnMove2 = '<br>Move 2: ' + bagPokemon[users[i]][x].inventory_item_data.pokemon_data.move_2;
+          ivAtk = bagPokemon[users[i]][x].inventory_item_data.pokemon_data.individual_attack;
+          ivDef = bagPokemon[users[i]][x].inventory_item_data.pokemon_data.individual_defense;
+          ivSta = bagPokemon[users[i]][x].inventory_item_data.pokemon_data.individual_stamina;
+          if (ivAtk === undefined) { ivAtk = 0; }
+          if (ivDef === undefined) { ivDef = 0; }
+          if (ivSta === undefined) { ivSta = 0; }
+          ivList = '<td>Attack: ' + ivAtk + '<br>Defense: ' + ivDef + '<br>Stamina: ' + ivSta + '</td>';
+          pokeballUsed = '<td><img src="image/items/' + bagPokemon[users[i]][x].inventory_item_data.pokemon_data.pokeball + '.png" class="png_img_small"></td>';
+        }
+        out += '<tr><td><img src="image/pokemon/' + 
+                pkmnImage + 
+                '" class="png_img"></td><td class="left-align">Name: ' + 
+                pkmnName +
+                '<br>Number: ' + 
+                pkmnNum + 
+                pkmnCP +
+                pkmnMove1 +
+                pkmnMove2 +
+                '</td>' +
+                ivList +
+                pokeballUsed +
+                '</tr>';
+      }
+      out += '</table></div>';
+    }
+    out += '</div>';
     document.getElementById('subcontent').innerHTML = out;
+    $('ul.tabs').tabs();
   }
   if (menu == 4) {
     document.getElementById('subtitle').innerHTML = "Pokedex";
-    pkmnTotal = pokedex.length;
-    out = '<div class="row items"><div class="col s12"><h5>' + users[0] + ' | ' + pkmnTotal + ' / 151</h5><table>';
-    for (i = 0; i < pokedex.length; i++) {
-      pkmnNum = pokedex[i].inventory_item_data.pokedex_entry.pokedex_entry_number;
-      pkmnImage = pad_with_zeroes(pokedex[i].inventory_item_data.pokedex_entry.pokedex_entry_number, 3) +'.png';
-      pkmnName = pokemonArray[pkmnNum-1].Name;
-      out += '<tr><td><img src="image/pokemon/' +
-              pkmnImage +
-              '" class="png_img"></td><td class="left-align">Name: ' +
-              pkmnName +
-              '<br>Number: ' +
-              pkmnNum +
-              '<br>Times Encountered: ' +
-              pokedex[i].inventory_item_data.pokedex_entry.times_encountered + 
-              '<br>Times Caught: ' +
-              pokedex[i].inventory_item_data.pokedex_entry.times_captured +
-              '</td></tr>';
+    out = '<div class="col s12"><ul class="tabs">';
+    for (i = 0; i < users.length; i++) {
+      out += '<li class="tab col s3"><a href="#tabP' +
+              users[i] +
+              '">' +
+              users[i] +
+              '</a></li>';
     }
-    out += '</table></div></div>';
+    out += '</ul></div>';
+    for (i = 0; i < users.length; i++) {
+      pkmnTotal = pokedex[users[i]].length;
+      out += '<div id="tabP' +
+            users[i] +
+            '" class="col s12 items"><h5>' + users[i] + ' | ' + pkmnTotal + ' / 151</h5><table>';
+      for (x = 0; x < pokedex[users[i]].length; x++) {
+        pkmnNum = pokedex[users[i]][x].inventory_item_data.pokedex_entry.pokedex_entry_number;
+        pkmnImage = pad_with_zeroes(pokedex[users[i]][x].inventory_item_data.pokedex_entry.pokedex_entry_number, 3) +'.png';
+        pkmnName = pokemonArray[pkmnNum-1].Name;
+        out += '<tr><td><img src="image/pokemon/' +
+                pkmnImage +
+                '" class="png_img"></td><td class="left-align">Name: ' +
+                pkmnName +
+                '<br>Number: ' +
+                pkmnNum +
+                '<br>Times Encountered: ' +
+                pokedex[users[i]][x].inventory_item_data.pokedex_entry.times_encountered + 
+                '<br>Times Caught: ' +
+                pokedex[users[i]][x].inventory_item_data.pokedex_entry.times_captured +
+                '</td></tr>';
+      }
+      out += '</table></div>';
+    }
+    out += '</div>';
     document.getElementById('subcontent').innerHTML = out;
+    $('ul.tabs').tabs();
   }
 }
