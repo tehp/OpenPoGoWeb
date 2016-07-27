d5c040f8eb652696330c98747531f684a8104546
reaver01 <reaver_01@gmx.com>
2016-07-27 09:29:11 +0200

    put logs at beggining of log window

--- js/main.js
+++ js/main.js
@@ -816,7 +816,7 @@ var mapView = {
   log: function(log_object) {
     var currentDate = new Date();
     var time = ('0' + currentDate.getHours()).slice(-2) + ':' + ('0' + (currentDate.getMinutes())).slice(-2);
-    $("#logs-panel .card-content").append("<div class='log-item'>\
+    $("#logs-panel .card-content #logs").prepend("<div class='log-item'>\
   <span class='log-date'>" + time + "</span><p class='" + log_object.color + "'>" + log_object.message + "</p></div>");
     if (!$('#logs-panel').is(":visible")) {
       Materialize.toast(log_object.message, 3000);
